456cd3ef0b6675289b4269d02569756e68b491d2
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-21 15:33:46 -0300

    test removeStorageCity

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -1,6 +1,12 @@
 import { CityProps } from "@services/getCityByNameService"
-import { getStorageCity, saveStorageCity } from "./cityStorage"
+import { getStorageCity, removeStorageCity, saveStorageCity } from "./cityStorage"
 
+const newCity: CityProps = {
+  id: '1',
+  name: 'São Paulo',
+  latitude: 123,
+  longitude: 456
+}
 describe('Storage: CityStorage', () => {
   it("should be return null when don't have a city storaged", async () => {
     const response = await getStorageCity()
@@ -8,16 +14,17 @@ describe('Storage: CityStorage', () => {
   })
 
   it("should be return city storaged", async () => {
-    const newCity: CityProps = {
-      id: '1',
-      name: 'São Paulo',
-      latitude: 123,
-      longitude: 456
-    }
     await saveStorageCity(newCity)
 
     const response = await getStorageCity()
     expect(response).toEqual(newCity)
-  })  
+  })
+
+  it("should be remove city storaged", async () => {
+    await saveStorageCity(newCity)
+    await removeStorageCity()
+    const response = await getStorageCity()
+    expect(response).toBeNull()
+  })    
   
 })

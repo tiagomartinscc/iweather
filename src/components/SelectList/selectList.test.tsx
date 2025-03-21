027ef29136dce832a26625ce072ed7f25ac613e4
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-21 10:34:28 -0300

    test empty list

--- src/components/SelectList/selectList.test.tsx
+++ src/components/SelectList/selectList.test.tsx
@@ -24,4 +24,18 @@ describe('Component: selectList', () => {
 
     expect(onPress).toHaveBeenCalledWith(data[0])
   })
+
+  it('not should be show options when data props is empty', () => {
+    render(
+      <SelectList
+        data={[]}
+        onChange={() => {}}
+        onPress={() => {}}
+      />
+    )
+
+    const options = screen.getByTestId('options')
+
+    expect(options.children).toHaveLength(0)
+  })
 })

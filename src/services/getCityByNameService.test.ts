0194f05058371b27bc72a87765d1159e3845b956
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-21 14:39:21 -0300

    create center mock

--- src/services/getCityByNameService.test.ts
+++ src/services/getCityByNameService.test.ts
@@ -1,21 +1,9 @@
 import { api } from './api'
 import { getCityByNameService } from './getCityByNameService'
+import { mockCityAPIResponse } from '@__tests__/mocks/mockCityAPIResponse'
 describe('API: getCityByNameService', () => {
   it('should return city details by name', async () => {
-    const data = {
-      data: {
-        id: '1',
-        name: 'São Paulo',
-        sys: {
-          country: 'BR'
-        },
-        coord: {
-          lat: 123,
-          lon: 456
-        }
-      }
-    }
-    jest.spyOn(api, 'get').mockResolvedValue(data)
+    jest.spyOn(api, 'get').mockResolvedValue({data: mockCityAPIResponse})
 
     const response = await getCityByNameService('São Paulo')
     expect(response.length).toBeGreaterThan(0)

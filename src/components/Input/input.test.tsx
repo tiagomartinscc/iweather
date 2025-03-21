422abd452c5a72307a6abb10d753a9d8cfb181bc
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-21 10:01:15 -0300

    test using toBeTruthy

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -4,9 +4,16 @@ import { Input } from "@components/Input"
 
 describe('Component: Input', () => {
   it ('should be render without activity indicator if isLoading prop is undefined', () => {
-    render(<input />)
+    render(<Input />)
 
     const activityIndicator = screen.queryByTestId('activity-indicator')
     expect(activityIndicator).toBeNull()
   })
+
+  it ('should be render with activity indicator if isLoading prop is true', () => {
+    render(<Input isLoading />)
+
+    const activityIndicator = screen.queryByTestId('activity-indicator')
+    expect(activityIndicator).toBeTruthy()
+  })  
 })

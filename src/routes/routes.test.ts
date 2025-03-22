d8557c9c41ce6d97c37b0deceff18f21ab7d181b
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-21 22:18:10 -0300

    routes test

---
+++ src/routes/routes.test.ts
@@ -0,0 +1,12 @@
+import { render, screen, waitFor } from "@testing-library/react-native"
+import { Routes } from "."
+
+describe("Routes", () => {
+  it.only('should be render Search screen when not city selecte', async () => {
+    render(<Routes />)
+
+    const title = await waitFor(() => screen.findByText(/^escolha um local/i))
+
+    expect(title).toBeTruthy()
+  })
+})

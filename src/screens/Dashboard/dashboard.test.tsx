de678fc17107c42f735baf4ab8531ae33257a5ee
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-24 16:41:00 -0300

    fix: test

--- src/screens/Dashboard/dashboard.test.tsx
+++ src/screens/Dashboard/dashboard.test.tsx
@@ -19,11 +19,13 @@ describe("Screen: Dashboard", () => {
   })
 
   it('should be show city weather', async () => {
-    jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse });    
+    jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse });
+
     render(<Dashboard />)
 
-    const cityName = await waitFor(() => screen.findByText(/rio do sul/i));
-    expect(cityName).toBeTruthy() 
+    await waitFor(() => expect(
+      screen.findByText(/rio do sul/i, {}, { timeout: 3000 })
+    ).toBeTruthy);
   })
 
   it('should be show another selected weather city', async () =>{
